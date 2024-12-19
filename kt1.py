cd3f3c3e3e5145edf31f8d91e15567c7525653e9
Rasmus Ojala <rasmus@hkhk.edu.ee>
2024-12-19 14:00:35 +0200

    Add files via upload

--- kt1.py
+++ kt1.py
@@ -33,16 +33,41 @@
 
 #7
 
-def currency_converter():
-    conversion = input('Sisestage väärtus eurodes, mis teisendatakse eekideks:')
-    EEK = 8.09647
-    error_message = 'Viga: teie sisend peaks olema positiivne arv'
+#def currency_converter(UserInput): 
+#    EEK = 8.09647
+#    error_message = 'Viga: teie sisend peaks olema positiivne arv'
+#    
+#    try:
+#        EUR = float(UserInput)*EEK
+#        print(f"Teie panus on võrdne {EUR} kividega")
+#    except:
+#        print(error_message)
+#
+#
+#conversion = input('Sisestage väärtus eurodes, mis teisendatakse EEK-i:')
+#currency_converter(conversion)
 
-    if (conversion.isdigit() == False):
-        return(error_message)
-    elif (conversion.isdecimal() == False):
-        return (error_message)
-    else:
-        conversion = float(conversion) * EEK
-        print("Teie sisend on võrdne {output} stones".format(output=conversion))
-        return conversion
+
+
+#8
+with open('palk.txt', 'r') as file:
+
+    palgad = []
+
+for line in file:
+
+    emp_id, sugu, palk = line.lower().strip().split(",")
+
+s = float(palk)
+
+palgad.append(s)
+
+Keskmine = sum(palgad)/len(palgad)
+
+Keskmine_mees = sum(palgad)/len(palgad)
+
+print("Palgad kokku = ",sum(palgad))
+
+print("Keskmised palgad = ", Keskmine)
+
+print("Keskmise meessoo palgad = ", Keskmine_mees)
